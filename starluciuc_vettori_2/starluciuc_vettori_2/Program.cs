5a653bd5e1b19aa35ddba8c27370804858d15091
starluciucabel <98151541+starluciucabel@users.noreply.github.com>
2022-03-11 10:20:26 +0100

    consegna primo es

---
+++ starluciuc_vettori_2/starluciuc_vettori_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace starluciuc_vettori_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ vettori_star/vettori_star/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vettori_star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 400;
+            int i = 0;
+            Random generatore = new Random();  
+            for (i = 0; i < dimensione; i++)
+            {
+                int numerocasuale = generatore.Next(0, 101);
+                Console.WriteLine($"{numerocasuale}");
+            }
+
+            
+        }
+    }
+}
