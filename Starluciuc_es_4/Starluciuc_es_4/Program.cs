c414eb973dffb57f8f75becd95b5ba81795388a1
starluciucabel <98151541+starluciucabel@users.noreply.github.com>
2022-03-25 10:12:34 +0100

    esercizione numero 4 dei vettori

---
+++ Starluciuc_es_4/Starluciuc_es_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Starluciuc_es_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 100;
+            int massimo = 0;
+            int minimo = int.MaxValue;
+            int[] vett = new int[dimensione];
+            for(int a = 0; a < dimensione; a++)
+            {
+                Random generatore = new Random();
+                int numero = generatore.Next(1000, 3000);
+                vett[a] = numero;
+                if (minimo > vett[a])
+                {
+                    minimo = vett[a];
+                }
+                if (massimo < vett[a])
+                {
+                    massimo = vett[a];
+                }
+            }
+            Console.WriteLine($"il più grande e :{massimo} il più piccolo è: {minimo}");
+        }
+    }
+}
